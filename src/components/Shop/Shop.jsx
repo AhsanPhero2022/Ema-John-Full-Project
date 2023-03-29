d232b327f518b106b71437c32a1e97c39fa05e8b
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-03-29 14:05:22 +0600

    components and products added

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import "./Shop.css";
+import { useState } from "react";
+import Product from "../Product/Product";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+
+      <div className="cart-container">
+        <h4>order summary</h4>
+        <p>Selected Items: {cart.length}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
